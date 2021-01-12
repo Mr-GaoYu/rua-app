469278a0002bd1328f9986cff67a434cba5f5599
Mr_GaoYu <Mr_GaoYu@163.com>
2021-01-12 21:02:47 +0800

    56

--- src/utilities/ReactChildren.tsx
+++ src/utilities/ReactChildren.tsx
@@ -1,2 +1,21 @@
 import React from "react";
 
+export const some = (children: React.ReactNode, method: Function, context?: any) => {
+    let index = 0;
+    let result = false;
+  
+    React.Children.forEach(children, child => {
+      if (result) {
+        return;
+      }
+      if (!React.isValidElement(child)) {
+        return;
+      }
+  
+      if (method.call(context, child, (index += 1))) {
+        result = true;
+      }
+    });
+  
+    return result;
+}
